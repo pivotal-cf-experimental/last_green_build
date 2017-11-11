49c406817842705c4aaa9521da29c9d568ccb9e0
Tom Dunlap <tdunlap@pivotal.io>
2017-11-11 01:48:23 -0800

    Initial commit

---
+++ last_green_build.rb
@@ -0,0 +1,61 @@
+require 'webrick'
+require 'net/http'
+require 'json'
+require 'openssl'
+
+class LastGreenBuild
+
+  def initialize(target:, pipeline:, job:, username:, password:, skip_ssl:)
+    @target = target
+    @pipeline = pipeline
+    @job = job
+    @username = username
+    @password = password
+    @skip_ssl = skip_ssl
+  end
+
+  def fetch_token
+    uri = URI("https://#{@target}/api/v1/teams/main/auth/token")
+    req = Net::HTTP::Get.new(uri)
+    req.basic_auth(@username, @password)
+    res = make_request(req)
+    JSON.parse(res.body)
+  end
+
+  def fetch_job_info(token)
+    uri = URI("https://#{@target}/api/v1/teams/main/pipelines/#{@pipeline}/jobs/#{@job}")
+    req = Net::HTTP::Get.new(uri)
+    req['Authorization'] = "#{token['type']} #{token['value']}"
+    res = make_request(req)
+    JSON.parse(res.body)
+  end
+
+  def make_request(req)
+    uri = req.uri
+    Net::HTTP.start(uri.hostname, uri.port, use_ssl: true, verify_mode: verify_mode) { |http| http.request(req) }
+  end
+
+  def verify_mode
+    @skip_ssl.nil? ? OpenSSL::SSL::VERIFY_PEER : OpenSSL::SSL::VERIFY_NONE
+  end
+
+  def human_readable_time_since(time)
+    secs = (Time.now - time).to_i
+    [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
+      if secs > 0
+        secs, n = secs.divmod(count)
+        "#{n.to_i} #{name}"
+      end
+    }.compact.reverse.join(' ')
+  end
+
+  def call(env)
+    token = fetch_token
+    job_info = fetch_job_info(token)
+    last_completed = job_info['finished_build']['end_time']
+
+    res = Rack::Response.new
+    res.write "#{human_readable_time_since(last_completed)}\n...since the last green build"
+    res.finish
+  end
+end
