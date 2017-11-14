a77b45f2c507ac56e906c2d58c6dcdad00d22106
Brian Cunnie <bcunnie@pivotal.io>
2017-11-13 18:20:26 -0800

    WIP
    Signed-off-by: Tom Dunlap <tdunlap@pivotal.io>

--- api/config.ru
+++ api/config.ru
@@ -3,6 +3,7 @@ require './last_green_build.rb'
 target = ENV['TARGET'] or raise 'missing TARGET environment variable: the url of the concourse ATC to check'
 pipeline = ENV['PIPELINE'] or raise 'missing PIPELINE environment variable: the name of the pipeline to check'
 job = ENV['JOB'] or raise 'missing JOB environment variable: the name of the job to check'
+resource = ENV['RESOURCE'] or raise 'missing RESOURCE environment variable: the name of the resource to check'
 username = ENV['CONCOURSE_USERNAME'] or raise 'missing CONCOURSE_USERNAME environment variable: basic auth user for this concourse'
 password = ENV['CONCOURSE_PASSWORD'] or raise 'missing CONCOURSE_PASSWORD environment variable: basic auth password for user'
 skip_ssl = ENV['SKIP_SSL']
@@ -11,6 +12,7 @@ run LastGreenBuild.new(
   target: target,
   pipeline: pipeline,
   job: job,
+  resource: resource,
   username: username,
   password: password,
   skip_ssl: skip_ssl)
--- api/last_green_build.rb
+++ api/last_green_build.rb
@@ -5,48 +5,64 @@ require 'openssl'
 
 class LastGreenBuild
 
-  def initialize(target:, pipeline:, job:, username:, password:, skip_ssl:)
+  def initialize(target:, pipeline:, job:, resource:, username:, password:, skip_ssl:)
     @target = target
     @pipeline = pipeline
     @job = job
+    @resource = resource
     @username = username
     @password = password
     @skip_ssl = skip_ssl
   end
+  
+  def call(env)
+    fetch_token!
+    job_info = fetch_job_info
+    last_build_id = job_info['finished_build']['id']
+    last_completed = job_info['finished_build']['end_time']
+    resource_list = get_resources_of_build(build_id: last_build_id)
+    last_green_sha = resource_list['inputs'].find{ |i| i['name'] == @resource }['version']['ref']
+    resource_versions = get_resource_version_list
 
-  def fetch_token
+    res = Rack::Response.new
+    res.set_header('Access-Control-Allow-Origin', '*')
+    res.write("#{last_completed}: #{last_build_id}: #{last_green_sha}")
+    res.finish
+  end
+  
+  private
+  
+  def fetch_token!
     uri = URI("https://#{@target}/api/v1/teams/main/auth/token")
     req = Net::HTTP::Get.new(uri)
     req.basic_auth(@username, @password)
-    res = make_request(req)
-    JSON.parse(res.body)
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true, verify_mode: verify_mode) { |http| http.request(req) }
+    @token = JSON.parse(res.body)
   end
 
-  def fetch_job_info(token)
-    uri = URI("https://#{@target}/api/v1/teams/main/pipelines/#{@pipeline}/jobs/#{@job}")
+  def fetch_job_info
+    make_request("https://#{@target}/api/v1/teams/main/pipelines/#{@pipeline}/jobs/#{@job}")
+  end
+
+  def make_request(url)
+    uri = URI(url)
     req = Net::HTTP::Get.new(uri)
-    req['Authorization'] = "#{token['type']} #{token['value']}"
-    res = make_request(req)
+    req['Authorization'] = "#{@token['type']} #{@token['value']}" if @token
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true, verify_mode: verify_mode) { |http| http.request(req) }
     JSON.parse(res.body)
   end
 
-  def make_request(req)
-    uri = req.uri
-    Net::HTTP.start(uri.hostname, uri.port, use_ssl: true, verify_mode: verify_mode) { |http| http.request(req) }
+  def get_resources_of_build(build_id:)
+    # https://om.ci.cf-app.com/api/v1/builds/31830/resources
+    make_request("https://#{@target}/api/v1/builds/#{build_id}/resources")
+  end
+
+  def get_resource_version_list
+    # https://om.ci.cf-app.com/api/v1/teams/main/pipelines/opsmanager-master/resources/installation/versions?limit=100
+    make_request("https://#{@target}/api/v1/teams/main/pipelines/#{@pipeline}/resources/#{@resource}/versions?limit=100")
   end
 
   def verify_mode
     @skip_ssl.nil? ? OpenSSL::SSL::VERIFY_PEER : OpenSSL::SSL::VERIFY_NONE
-  end
-
-  def call(env)
-    token = fetch_token
-    job_info = fetch_job_info(token)
-    last_completed = job_info['finished_build']['end_time']
-
-    res = Rack::Response.new
-    res.set_header('Access-Control-Allow-Origin', '*')
-    res.write last_completed
-    res.finish
   end
 end
